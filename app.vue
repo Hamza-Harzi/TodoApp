e2e633aa9d3d0651fb4d90b660bcc3b7e9bf5cf7
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-16 18:24:20 +0200

    Making POST, PUT, DELETE Requests From the Frontend

--- app.vue
+++ app.vue
@@ -1,3 +1,63 @@
+<script setup lang="ts">
+const input = ref("");
+const { data: todos } = useFetch("/api/todo");
+const addTodo = async () => {
+  if (!input) return;
+  await $fetch("/api/todo", { method: "post", body: { item: input.value } });
+};
+const updateTodo = async (id) => {
+  await $fetch(`/api/todo/${id}`, { method: "put" });
+};
+const deleteTodo = async (id) => {
+  await $fetch(`/api/todo/${id}`, { method: "delete" });
+};
+</script>
+
 <template>
-  <div></div>
+  <div class="container">
+    <NCard class="cards">
+      <h1>My Todos</h1>
+      <div class="add-todo">
+        <input placeholder="Add a new todo..." v-model="input" />
+        <NButton @click="addTodo">Add</NButton>
+      </div>
+      <NCard
+        @click="() => updateTodo(todo.id)"
+        class="card"
+        v-for="todo in todos"
+        :key="todo.id"
+      >
+        <h4 :class="todo.completed ? 'complete' : null">{{ todo.item }}</h4>
+        <p @click="() => deleteTodo(todo.id)">x</p>
+      </NCard>
+    </NCard>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  margin: 0 auto;
+  max-width: 50%;
+}
+.cards {
+  padding: 2rem;
+}
+input {
+  outline: none;
+}
+.add-todo {
+  display: flex;
+  justify-content: space-between;
+}
+.card {
+  padding: 0.5rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>
